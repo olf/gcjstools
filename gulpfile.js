bc3753449c958ef51b0deb1533eefeb2f1c795d0
Olaf Peters <olf@magicolf.de>
2016-02-21 14:13:56 +0100

    change linting to eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
+/* eslint-env node */
+
 var gulp = require('gulp'),
-	watch = require('gulp-watch'),
-	batch = require('gulp-batch'),
-	qunit = require('gulp-qunit');
+    watch = require('gulp-watch'),
+    batch = require('gulp-batch'),
+    qunit = require('gulp-qunit');
+
+gulp.task('default', ['watch']);
 
 gulp.task('test', function() {
-	return gulp
-		.src('./tests/index.html')
-		.pipe(qunit());
+    return gulp
+        .src('./tests/index.html')
+        .pipe(qunit());
 });
 
 gulp.task('watch', function () {
