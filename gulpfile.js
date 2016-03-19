fd4aca21e8fbedb568860798d51b9f64721222e8
Olaf Peters <olf@magicolf.de>
2016-03-19 12:46:51 +0100

    add lint task to build testing (and introduce minor liniting error to test travis task)

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 /* eslint-env node */
 
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 var tape = require('gulp-tape');
 var tapReporter = require('faucet');
 
 gulp.task('default', ['watch']);
 
-gulp.task('test', function() {
+gulp.task('unittest', function() {
     return gulp.src('tests/**/*.js')
         .pipe(tape({
             reporter: tapReporter()
         }));
 });
 
+gulp.task('lint', function () {
+    return gulp.src(['**/*.js','!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
 gulp.task('watch', function () {
-    gulp.watch('**/*.js', ['test']);
+    gulp.watch('**/*.js', ['lint', 'unittest']);
 });
