8fe2c53d96903e5e4f1a4170055ac319c40c1dbd
Olaf Peters <olf@magicolf.de>
2021-06-05 18:16:44 +0200

    Update dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,26 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var tape = require('gulp-tape');
-var tapReporter = require('faucet');
 
-gulp.task('default', ['watch']);
-
-gulp.task('unittest', function() {
+gulp.task('unittest', function () {
     return gulp.src('tests/**/*.js')
-        .pipe(tape({
-            reporter: tapReporter()
-        }));
+        .pipe(tape());
 });
 
 gulp.task('lint', function () {
-    return gulp.src(['**/*.js','!node_modules/**'])
+    return gulp.src(['**/*.js', '!node_modules/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
 gulp.task('watch', function () {
-    gulp.watch('**/*.js', ['lint', 'unittest']);
+    gulp.watch('**/*.js', gulp.series('lint', 'unittest'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch('./sass/**/*.scss', gulp.series('lint', 'unittest'));
+});
+
+gulp.task('default', gulp.parallel('watch'));
+
